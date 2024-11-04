411d6a3f69143129f8453fa90b39be8ec2b0bd59
El Toro Maestro <eltoro@atomlogik.de>
2024-11-04 11:10:02 -0500

    demonstrated we can call things with the new nomad

--- app/Main.hs
+++ app/Main.hs
@@ -5,15 +5,26 @@ import NEAT
 main :: IO ()
 main = do
   let iconfig = initialConfig
-  let (s1, s2, s3) = evalState (
+  let (s1, s2, s3, ar) = evalState (
         do 
           s1 <- nextSequenceNumber 
           s2 <- nextSequenceNumber 
-          s3 <- nextSequenceNumber 
-          return (s1, s2, s3)
+          s3 <- nextSequenceNumber
+          ar <- seqtest
+          return ( s1, s2, s3, ar)
         ) iconfig
  
   putStrLn $ "Hello, Haskell! " ++ show s1
     ++ " and " ++ show s2
     ++ " and " ++ show s3
-      
+    ++ " and the array " ++ show ar
+    where
+      seqtest :: SN [Int64]
+      seqtest = do
+        ss4 <- nextSequenceNumber
+        ss5 <- nextSequenceNumber
+        ss6 <- nextSequenceNumber
+        return [ss4, ss5, ss6]
+        
+
+
