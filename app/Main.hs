c64323449efec8c4743da182175522f37079fdf3
El Toro Maestro <eltoro@atomlogik.de>
2024-11-04 07:25:04 -0500

    need to create an actual monad instance

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,21 @@
 module Main where
 
 import NEAT
+import GHC.RTS.Flags (GCFlags(initialStkSize))
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
-
-
-
-
-
-
-
+main = do
+  let iconfig = initialConfig
+  let (s1, sn) = runState nextSequenceNumber iconfig
+  let (s2, sn) = runState nextSequenceNumber sn
+  let (s3, sn) = runState nextSequenceNumber sn
+  
+  putStrLn $ "Hello, Haskell! " ++ show s1
+    ++ " and " ++ show s2
+    ++ " and " ++ show s3
+  where
+    seq :: SN Int64
+    seq = do
+      config <- getConfig
+      return $ sequence_number config
+      
--- lib/SNMonad.hs
+++ lib/SNMonad.hs
@@ -1,15 +1,46 @@
-module SNMonad where
+module SNMonad ( module SNMonad
+               , module Control.Monad.State
+               , Int64
+               ) where
 
+import Data.Int (Int64)
 import Control.Monad.State
 import Genetics.Neurons
 import Evolution.Goals
 
-data Config = Config { population_size :: Int
+data Config = Config { population_size :: Int64
                      , neuron_types :: [Neuron]
                      , goal :: Goal
+                     , sequence_number :: Int64
                      } deriving Show
 
 type SN = State Config 
+
+initialConfig :: Config
+initialConfig = Config { population_size = 100
+                       , neuron_types = [Neuron]
+                       , goal = Goal
+                       , sequence_number = 0
+                       }
+
+getConfig :: SN Config
+getConfig = get
+
+updateConfig :: Config -> SN ()
+updateConfig newconf = put newconf
+
+nextSequenceNumber :: SN Int64
+nextSequenceNumber = do
+  config <- getConfig
+  let next_seq = sequence_number config
+  let uconf = config {sequence_number = next_seq + 1 }
+  updateConfig uconf
+  return next_seq
+
+
+
+
+
 
 
 
