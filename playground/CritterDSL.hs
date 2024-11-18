e80dc1d56af430f5425bd61092c3d95ac863eed3
El Toro Maestro <eltoro@atomlogik.de>
2024-11-18 07:42:21 -0500

    pushing this out to github so other eyes can look at the Hspec issue

--- playground/CritterDSL.hs
+++ playground/CritterDSL.hs
@@ -4,25 +4,30 @@ import NEAT
 
 main :: IO ()
 main = do
-  critter <- mkCritter [ node mkRegular Input  -- 0
-                      , node mkRegular Input  -- 1
-                      , node mkRegular Input  -- 2
-                      , node mkRegular Input  -- 3
-                      , node mkRegular Input  -- 4
-                      , node mkRegular Output -- 5
-                      , node mkRegular Output -- 6
-                      , node mkRegular Output -- 7
-                      ] [ conn 0 6
-                        , conn 1 6
-                        , conn 2 6
-                        , conn 3 7
-                        , conn 2 7
-                        , conn 4 5
-                        , conn 3 5
-                        , conn 0 5
-                        , conn 0 7
-                        , conn 4 7
-                        ]
+  cfg <- getConfig
+  critter <- evalStateT (
+    do 
+      critter' <- mkCritter [ node mkRegular Input  -- 0
+                           , node mkRegular Input  -- 1
+                           , node mkRegular Input  -- 2
+                           , node mkRegular Input  -- 3
+                           , node mkRegular Input  -- 4
+                           , node mkRegular Output -- 5
+                           , node mkRegular Output -- 6
+                           , node mkRegular Output -- 7
+                           ] [ conn 0 6
+                             , conn 1 6
+                             , conn 2 6
+                             , conn 3 7
+                             , conn 2 7
+                             , conn 4 5
+                             , conn 3 5
+                             , conn 0 5
+                             , conn 0 7
+                             , conn 4 7
+                             ]
+      return critter'
+    ) cfg
   putStrLn critter
   
 {-
--- test/Spec.hs
+++ test/Spec.hs
@@ -14,31 +14,10 @@ main :: IO ()
 main = hspec $ do
   describe "Prelude.head" $ do
     it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (24 :: Int)
+      head [23 ..] `shouldBe` (24 :: Int) -- this should fail!
 
     it "returns the first element of an *arbitrary* list" $
       property $ \x xs -> head (x:xs) == (x :: Int)
 
     it "throws an exception if used with an empty list" $ do
       evaluate (head []) `shouldThrow` anyException
-{-
-  crit <- mkCritter [ node Regular Input  -- 0
-                   , node Regular Input  -- 1
-                   , node Regular Input  -- 2
-                   , node Regular Input  -- 3
-                   , node Regular Input  -- 4
-                   , node Regular Output -- 5
-                   , node Regular Output -- 6
-                   , node Regular Output -- 7
-                   ] [ conn 0 6
-                     , conn 1 6
-                     , conn 2 6
-                     , conn 3 7
-                     , conn 2 7
-                     , conn 4 5
-                     , conn 3 5
-                     , conn 0 5
-                     , conn 0 7
-                     , conn 4 7
-                     ]
--}
