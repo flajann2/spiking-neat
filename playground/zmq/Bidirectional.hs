0255a2dd9d87f1af4e21f6dec2e42c2b00c14609
El Toro Maestro <eltoro@atomlogik.de>
2025-02-07 19:17:11 -0500

    ZMQ bidirectional working

--- playground/zmq/Bidirectional.hs
+++ playground/zmq/Bidirectional.hs
@@ -2,117 +2,37 @@
 
 module Main where
 
-import Control.Concurrent (forkIO)
+import Control.Monad
 import qualified Data.ByteString.Char8 as B
-import System.ZMQ4
+import System.ZMQ4 hiding (socket, bind, connect, send, receive)
 import System.ZMQ4.Monadic ( liftIO
                            , runZMQ
                            , socket
                            , bind
                            , send
                            , connect
-                           , receive )
+                           , receive
+                           , async)
 
 main :: IO ()
 main = runZMQ $ do
-  sender <- socket ZMQ_PUSH
-  receiver <- socket ZMQ_PULL
+  sender   <- socket Push
+  receiver <- socket Pull
+
   bind sender "tcp://*:5555"
   connect receiver "tcp://localhost:5555"
 
-  let sendLoop s = do
-      send s [] (B.pack "Hello")
-      sendLoop s
+  let sendLoop s i = do
+        send sender [] (B.pack $ s ++ " : " ++ show i)
+        sendLoop s (i+1)
 
-  let receiveLoop r = do
-      msg <- receive r
-      liftIO $ print msg
-      receiveLoop r
+  let receiveLoop = forever $ do
+        msg <- receive receiver
+        liftIO $ print msg
+        -- receiveLoop 
 
-  liftIO $ forkIO $ sendLoop sender
-  liftIO $ forkIO $ receiveLoop receiver
+  _ <- async $ sendLoop "World" 0
+  -- _ <- async $ receiveLoop
+  receiveLoop
+  
   return ()
-
---- import System.ZMQ4
---- import System.ZMQ4.Monadic ( liftIO
----                            , runZMQ
----                            , socket
----                            , bind
----                            , send
----                            , connect
----                            , receive )
---- import Control.Concurrent (forkIO)
---- import qualified Data.ByteString.Char8 as B
---- 
---- main :: IO ()
---- main = runZMQ $ do
----     withContext $ \ _ctx -> do
----         sender <- socket ZMQ_PUSH
----         receiver <- socket ZMQ_PULL
----         bind sender "tcp://*:5555"
----         connect receiver "tcp://localhost:5555"
---- 
----         let sendLoop s = do
----             send s [] (B.pack "Hello")
----             sendLoop s
---- 
----         let receiveLoop r = do
----             msg <- receive r
----             liftIO $ print msg
----             receiveLoop r
---- 
----         liftIO $ forkIO $ sendLoop sender
----         liftIO $ forkIO $ receiveLoop receiver
----         return ()
-
---- import Control.Concurrent
---- import Control.Monad
---- import Data.ByteString (ByteString)
---- import System.ZMQ4.Monadic
---- 
---- import System.ZMQ4 (withContext, Push, Pull)
---- main :: IO ()
---- main = withContext $ \ ctx -> do
----     let sender = socket ctx Push
----     receiver <- socket ctx Pull
---- 
----     bind sender "tcp://*:5555"
----     connect receiver "tcp://localhost:5555"
---- 
----     forkIO $ sendLoop ctx sender
----     receiveLoop ctx receiver
---- 
---- --sendLoop :: Context -> Socket -> IO ()
---- sendLoop ctx socket = forever $ do
----     send socket (pack "Hello from sender!") 0
---- 
---- --receiveLoop :: Context -> Socket -> IO ()
---- receiveLoop ctx socket = forever $ do
----     msg <- recv socket 0
----     putStrLn $ "Received: " ++ unpack msg
-
---- import Control.Concurrent
---- import Control.Monad
---- import Data.ByteString (ByteString)
---- import qualified System.ZMQ4 as Z -- (Context, withContext)
---- import System.ZMQ4.Monadic
---- 
---- main :: IO ()
---- main = Z.withContext $ \ctx -> do
----     sender <- socket ctx PUSH
----     receiver <- socket ctx PULL
---- 
----     bind sender "tcp://*:5555"
----     connect receiver "tcp://localhost:5555"
---- 
----     forkIO $ sendLoop ctx sender
----     receiveLoop ctx receiver
---- 
---- sendLoop :: Z.Context -> Z.Socket -> IO ()
---- sendLoop ctx socket = forever $ do
----     send socket (pack "Hello from sender!") 0
---- 
---- receiveLoop :: Z.Context -> Z.Socket -> IO ()
---- receiveLoop ctx socket = forever $ do
----     msg <- recv socket 0
----     putStrLn $ "Received: " ++ unpack msg
