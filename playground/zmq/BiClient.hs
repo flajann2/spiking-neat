9d7b009bf407e77be8b3dfd0318209d9d5634f93
El Toro Maestro <eltoro@atomlogik.de>
2025-03-24 21:11:12 -0500

    working on bidirectional connection. It keeps failing

--- playground/zmq/BiClient.hs
+++ playground/zmq/BiClient.hs
@@ -1,23 +1,22 @@
 module Main where
 
 import Control.Monad
-import qualified System.ZMQ4 as Z
+import System.ZMQ4
 import qualified Data.ByteString.Char8 as C8
 
 main :: IO ()
-main = Z.withContext $ \ctx -> do
+main = withContext $ \ctx -> do
     -- Create a PAIR socket for bidirectional communication
-    socket <- Z.socket ctx Z.Pair
+    sock <- socket ctx Pair
     
     -- Connect the socket to the server's endpoint
-    Z.connect socket "tcp://localhost:5555"
+    connect sock "tcp://localhost:5555"
+
+    forever $ do
+      -- Send a message to the server
+      send sock [] $ C8.pack "Hello from client!"
+      putStrLn "sent message to server."
     
-    -- Send a message to the server
-    Z.send socket [] $ C8.pack "Hello from client!"
-    
-    -- Receive a response from the server
-    msg <- Z.receive socket
-    putStrLn $ "Received: " ++ show msg
-    
-    -- You can continue sending and receiving in a loop
-    -- for ongoing communication.
+      -- Receive a response from the server
+      msg <- receive sock
+      putStrLn $ "Received: " ++ show msg
--- playground/zmq/BiServer.hs
+++ playground/zmq/BiServer.hs
@@ -1,24 +1,25 @@
 module Main where
 
 import Control.Monad
-import qualified System.ZMQ4 as Z
+import System.ZMQ4
 import qualified Data.ByteString.Char8 as C8
 
 main :: IO ()
-main = Z.withContext $ \ctx -> do
+main = withContext $ \ctx -> do
     -- Create a PAIR socket for bidirectional communication
-    socket <- Z.socket ctx Z.Pair
+    sock <- socket ctx Pair
     
     -- Bind the socket to a TCP endpoint
-    Z.bind socket "tcp://*:5555"
+    bind sock "tcp://*:5555"
     
     putStrLn "Server listening on port 5555"
     
     -- Loop to receive and send messages
     forever $ do
         -- Receive a message
-        msg <- Z.receive socket
+        msg <- receive sock
         putStrLn $ "Received: " ++ show msg
         
         -- Send a response back
-        Z.send socket [] $ C8.pack "Hello from server!"
+        send sock [] $ C8.pack "Hello from server!"
+        putStrLn "sent response to client"
